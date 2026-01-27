3cc12a0d3390448976e15e21f84ae12186edad56
sebvu <santosjester738@gmail.com>
2026-01-26 19:34:38 -0600

    complete js data types and conditionals

--- foundations/04_mathEquations/mathEquations.js
+++ foundations/04_mathEquations/mathEquations.js
@@ -1,9 +1,15 @@
-const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
+let b = "one plus eight";
+let c = "22 times three";
+let d = "the *remainder* of 5/4";
+let e = "the variable 'b' minus 17";
+let f = "the sum of the previous five variables";
+
+b = 1 + 8;
+c = 22 * 3;
+d = 5 % 4;
+e = b - 17;
+f = b + c + d + e;
 
 // Do not edit below this line
-module.exports = {a, b, c, d, e, f}
+module.exports = { a, b, c, d, e, f };
--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,19 +7,27 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
-// Add your code right below, good luck!
+const firstName = "Carlos",
+  lastName = "Stevenson",
+  thisYear = 1965,
+  birthYear = 1947;
 
+const fullName = `${firstName} ${lastName}`,
+  age = thisYear - birthYear;
 
+// const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`;
 
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
 
+console.log(greeting);
 
 // Do not change this
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  firstName: typeof firstName === "undefined" ? undefined : firstName,
+  lastName: typeof lastName === "undefined" ? undefined : lastName,
+  thisYear: typeof thisYear === "undefined" ? undefined : thisYear,
+  birthYear: typeof birthYear === "undefined" ? undefined : birthYear,
+  greeting: typeof greeting === "undefined" ? undefined : greeting,
+  fullName: typeof fullName === "undefined" ? undefined : fullName,
+  age: typeof age === "undefined" ? undefined : age,
+};
