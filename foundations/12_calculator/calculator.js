8ad60a0198298b8dc428640c937ec681f209b1c5
Jester <jester.sebastiansantos@gmail.com>
2026-04-20 02:23:14 -0500

    yay

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, num) => num + acc, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, num) => num * acc, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let acc = 1;
+  for (let i = num; i > 0; i--) {
+    acc *= i;
+    console.log(i);
+  }
+  return acc;
 };
+
+factorial(5);
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
